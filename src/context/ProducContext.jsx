ae00e892cf7298e6780d3214beb5c2e952bd2693
amidshojaeinasab <amidshojaeinasab@gmail.com>
2025-10-20 12:49:39 +0330

    add axios and context folder

---
+++ src/context/ProducContext.jsx
@@ -0,0 +1,26 @@
+﻿import { createContext, useEffect, useState } from "react"
+import api from "../services/config"
+
+const ProdutContext = createContext()
+const [products , setProducts] = useState
+
+function ProductsProvider({children}) {
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                setProducts(await api.get("/products"))
+            } catch (error) {
+                console.log(error.message)
+            }
+        }
+    fetchProducts()
+    }, [])
+
+  return (
+    <ProdutContext.Provider value={products}>
+        {children}
+    </ProdutContext.Provider>
+  )
+}
+
+export default ProductsProvider
