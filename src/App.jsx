381c2049bdf7054e8094f8edad83cb333c291d05
amidshojaeinasab <amidshojaeinasab@gmail.com>
2025-10-23 10:46:28 +0330

    add card context

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,23 @@ import DetalisPage from "./Pages/DetalisPage"
 import CheckoutPage from "./Pages/CheckoutPage"
 import PageNotFound from "./Pages/404"
 import ProductsProvider from "./context/ProducContext"
+import CartProvider from "./context/CardContext"
 
 function App() {
  
 
   return (
-    <ProductsProvider>
-      <Routes>
-      <Route index element={<Navigate to="/products" replace />} />
-      <Route path="/products" element={<ProductsPage/>} />
-      <Route path="/products/:id" element={<DetalisPage/>} />
-      <Route path="/checkout" element={<CheckoutPage/>} />
-      <Route path="/*" element={<PageNotFound/>} />
-    </Routes>
-    </ProductsProvider>
+    <CartProvider>
+      <ProductsProvider>
+        <Routes>
+          <Route index element={<Navigate to="/products" replace />} />
+          <Route path="/products" element={<ProductsPage/>} />
+          <Route path="/products/:id" element={<DetalisPage/>} />
+          <Route path="/checkout" element={<CheckoutPage/>} />
+          <Route path="/*" element={<PageNotFound/>} />
+        </Routes>
+      </ProductsProvider>
+    </CartProvider>
   )
 }
 
---
+++ src/context/CardContext.jsx
@@ -0,0 +1,17 @@
+﻿import { Children, createContext, useReducer } from "react"
+
+const initialstate ={}
+const reducer = () =>{
+
+}
+
+const CartContext = createContext
+
+function CartProvider({children}) {
+    const [state , dispatch] = useReducer(reducer, initialstate)
+  return <CartContext.provider value={state}>
+        {children}
+  </CartContext.provider>
+}
+
+export default CartProvider
