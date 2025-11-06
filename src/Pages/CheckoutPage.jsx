7645ba4a96364e827b6ffa9325e4c06907e2a805
amidshojaeinasab <amidshojaeinasab@gmail.com>
2025-11-06 13:33:51 +0330

    add sidebar

--- src/Pages/CheckoutPage.jsx
+++ src/Pages/CheckoutPage.jsx
@@ -1,13 +1,22 @@
 ﻿import BasketCard from "../components/BasketCard"
 import { useCart } from "../context/CardContext"
+import styles from "./CheckoutPage.module.css"
 
 function CheckoutPage() {
   const [state, dispatch] = useCart()
   const clickHandler = (type, payload) => dispatch({type, payload})
 
+  if (!state.itemsCounter){
+    return(
+      <div className={styles.container}>
+        <p>Empty</p>
+      </div>
+    )
+  }
   return (
-    <div>
-      <div>
+    <div className={styles.container}>
+      <BasketCard state={state} clickHandler={clickHandler}/>
+      <div className={styles.products}>
         {state.selectedItems.map((product) =>(
           <BasketCard key={product.id} data={product} clickHandler={clickHandler} />
         ))}
